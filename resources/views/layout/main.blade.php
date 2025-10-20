83deb54e32c44cb6de8ee23153b10ba6fe72d5f0
MarcioParanhos <marcio.naga@gmail.com>
2025-10-20 15:44:10 -0300

    :art: style: Ajustar o padding dos botões e atualizar os ícones de navegação para maior clareza e consistência.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -415,16 +415,16 @@
                                         </li>
                                     @endif
                                     @if (Auth::user()->profile != 'consulta')
-                                        <li class="nav-item">
+                                        <li class="nav-item text-white">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                viewBox="0 0 24 24" fill="none" stroke="#f3f8fb" stroke-width="2"
-                                                stroke-linecap="round" stroke-linejoin="round"
-                                                class="icon icon-tabler icons-tabler-outline icon-tabler-user-code">
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                class="icon icon-tabler icons-tabler-outline icon-tabler-users">
                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                                                <path d="M6 21v-2a4 4 0 0 1 4 -4h3.5" />
-                                                <path d="M20 21l2 -2l-2 -2" />
-                                                <path d="M17 17l-2 2l2 2" />
+                                                <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+                                                <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
                                             </svg>
 
                                             <a class="nav-link sub-title"
